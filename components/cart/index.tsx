77f871459544e64ad508dfbfb5682aa0b4c3ba3d
emmanuelandyeze <emmanuel.andyeze@gmail.com>
2023-10-16 01:28:28 +0100

    first commit

--- components/cart/index.tsx
+++ components/cart/index.tsx
@@ -1,9 +1,10 @@
 import { getCart } from 'lib/shopify';
+import { cookies } from 'next/headers';
 import CartModal from './modal';
 
 export default async function Cart() {
-  const cartId = null
-  // cookies().get('cartId')?.value;
+  const cartId = cookies().get('cartId')?.value;
+ 
   let cart;
 
   if (cartId) {
